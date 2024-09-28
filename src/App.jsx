d13831a4713d798206954267184e41e35494e271
Jakub <jakubpachut@gmail.com>
2024-09-28 02:04:50 +0200

    finish app

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,66 @@ function App() {
   const [blocks, setblocks] = useState(
     shuffleSymbols.map((symbol) => ({ symbol, reveal: false, matched: false })),
   );
-  // const [firstPick, setFirstPick] = useState(null);
-  // const [secondPick, setSecondPick] = useState(null);
+  const [firstPick, setFirstPick] = useState(null);
+  const [secondPick, setSecondPick] = useState(null);
+  const [isComparing, setIsComapring] = useState(false);
+  const [points, setPoints] = useState(0);
 
-  function revealBlock(index) {
-    if(!blocks[index].reveal && !blocks[index].matched)
+  function handleBlockClick(index) {
+    if (isComparing || blocks[index].reveal || blocks[index].matched) return;
+
     setblocks((prevBlocks) =>
       prevBlocks.map((block, i) =>
         i === index ? { ...block, reveal: true } : block,
       ),
     );
+
+    if (firstPick === null) {
+      setFirstPick(index);
+    } else if (secondPick === null) {
+      setSecondPick(index);
+      setIsComapring(true);
+      compareBlocks(firstPick, index);
+    }
+  }
+
+  function compareBlocks(first, second) {
+    let newBlocks;
+    if (blocks[first].symbol === blocks[second].symbol) {
+      newBlocks = blocks.map((block, i) =>
+        i === first || i === second ? { ...block, matched: true } : block,
+      );
+      setPoints((prevPoints) => prevPoints + 1);
+      setblocks(newBlocks);
+      setIsComapring(false);
+      setFirstPick(null);
+      setSecondPick(null);
+    } else {
+      newBlocks = blocks.map((block, i) =>
+        i === first || i === second ? { ...block, reveal: false } : block,
+      );
+      setTimeout(() => {updateState(newBlocks)}, 1000);
+      if (points === symbols.length) {
+        alert("You win!");
+      }
+    }
+  }
+
+  function updateState(newBlocks) {
+    setblocks(newBlocks);
+    setIsComapring(false);
+    setFirstPick(null);
+    setSecondPick(null);
   }
 
   return (
     <div className="flex flex-col justify-center bg-slate-800 text-white min-h-screen w-full">
-      <h1 className="text-center text-3xl">Ruch gracza 1</h1>
-      <div className="grid grid-cols-[repeat(4,_minmax(100px,150px))] gap-4 place-content-center mx-auto px-4 w-full max-w-[1200px] mt-16">
+      <h1 className="text-center text-3xl">Memory game</h1>
+      <div className="grid grid-cols-[repeat(4,_minmax(100px,150px))] gap-6 place-content-center mx-auto px-4 w-full max-w-[1200px] mt-16">
         {blocks.map((block, index) => (
           <div
-            className="p-8 text-center grid place-content-center text-[2rem]  bg-emerald-500 rounded-lg cursor-pointer"
-            onClick={() => revealBlock(index)}
+            className={`p-8 text-center grid transition-all place-content-center text-[2rem]  bg-emerald-500 rounded-lg cursor-pointer hover:bg-emerald-400 ${block.reveal || block.matched ? "scale-110 bg-slate-400 hover:bg-slate-400" : ""}`}
+            onClick={() => handleBlockClick(index)}
             key={index}
           >
             {block.reveal || block.matched ? block.symbol : "❓"}
@@ -35,18 +75,15 @@ function App() {
       </div>
       <span className="text-center">
         <button
-          className="mt-10 py-4 px-8 bg-sky-600 text-xl rounded-lg"
-          onClick={() => setblocks()}
+          className="mt-10 py-4 px-8 bg-sky-600 text-xl rounded-lg transition-colors hover:bg-sky-500"
+          onClick={() => window.location.reload()}
         >
           Reset
         </button>
       </span>
       <div className="text-center mt-5 text-4xl">
         <p className="mb-5">
-          Player 1: <span>0</span>
-        </p>
-        <p>
-          Player 2: <span>0</span>
+          Points: <span>{points}</span>
         </p>
       </div>
     </div>
