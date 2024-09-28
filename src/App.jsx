ca08d9bde6513d4451ef092319ee9db12a2f5f97
Jakub <jakubpachut@gmail.com>
2024-09-28 02:22:40 +0200

    small improvements

--- src/App.jsx
+++ src/App.jsx
@@ -37,15 +37,14 @@ function App() {
         i === first || i === second ? { ...block, matched: true } : block,
       );
       setPoints((prevPoints) => prevPoints + 1);
-      setblocks(newBlocks);
-      setIsComapring(false);
-      setFirstPick(null);
-      setSecondPick(null);
+      updateState(newBlocks);
     } else {
       newBlocks = blocks.map((block, i) =>
         i === first || i === second ? { ...block, reveal: false } : block,
       );
-      setTimeout(() => {updateState(newBlocks)}, 1000);
+      setTimeout(() => {
+        updateState(newBlocks);
+      }, 1000);
     }
   }
 
@@ -62,30 +61,36 @@ function App() {
 
   return (
     <div className="flex flex-col justify-center bg-slate-800 text-white min-h-screen w-full">
-      <h1 className="text-center text-3xl">Memory game</h1>
-      <div className="grid grid-cols-[repeat(4,_minmax(100px,150px))] gap-6 place-content-center mx-auto px-4 w-full max-w-[1200px] mt-16">
-        {blocks.map((block, index) => (
-          <div
-            className={`p-8 text-center grid transition-all place-content-center text-[2rem]  bg-emerald-500 rounded-lg cursor-pointer hover:bg-emerald-400 ${block.reveal || block.matched ? "scale-110 bg-slate-400 hover:bg-slate-400" : ""}`}
-            onClick={() => handleBlockClick(index)}
-            key={index}
+      <h1 className="text-center block sm:hidden">
+        This game is not designed for smartphones, please play on a computer or
+        tablet{" "}
+      </h1>
+      <div className="hidden sm:block">
+        <h1 className="text-center text-3xl">Memory game</h1>
+        <div className="grid grid-cols-[repeat(4,_minmax(100px,150px))] gap-6 place-content-center mx-auto px-4 w-full max-w-[1200px] mt-16">
+          {blocks.map((block, index) => (
+            <div
+              className={`p-8 text-center grid transition-all place-content-center text-[2rem]  bg-emerald-500 rounded-lg cursor-pointer hover:bg-emerald-400 ${block.reveal || block.matched ? "scale-110 bg-slate-400 hover:bg-slate-400" : ""}`}
+              onClick={() => handleBlockClick(index)}
+              key={index}
+            >
+              {block.reveal || block.matched ? block.symbol : "❓"}
+            </div>
+          ))}
+        </div>
+        <span className="block text-center">
+          <button
+            className="mt-10 py-4 px-8 bg-sky-600 text-xl rounded-lg transition-colors hover:bg-sky-500"
+            onClick={() => window.location.reload()}
           >
-            {block.reveal || block.matched ? block.symbol : "❓"}
-          </div>
-        ))}
-      </div>
-      <span className="text-center">
-        <button
-          className="mt-10 py-4 px-8 bg-sky-600 text-xl rounded-lg transition-colors hover:bg-sky-500"
-          onClick={() => window.location.reload()}
-        >
-          Reset
-        </button>
-      </span>
-      <div className="text-center mt-5 text-4xl">
-        <p className="mb-5">
-          Points: <span>{points}</span>
-        </p>
+            Reset
+          </button>
+        </span>
+        <div className="text-center mt-5 text-4xl">
+          <p className="mb-5">
+            Points: <span>{points}</span>
+          </p>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      sm: "500px",
+    },
   },
   plugins: ["prettier-plugin-tailwindcss"],
 };
